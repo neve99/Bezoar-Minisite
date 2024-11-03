9aa2f636b474bf86bb3bfb004eeba39b657ce772
neve99 <165163178+neve99@users.noreply.github.com>
2024-11-03 21:07:22 +0100

    update scroll js

--- scroll.js
+++ scroll.js
@@ -1,3 +1,5 @@
+
+//backgroumd change
 document.addEventListener('scroll', function () {
   const section2 = document.querySelector('.s2');
   const section2Top = section2.getBoundingClientRect().top;
@@ -10,3 +12,25 @@ document.addEventListener('scroll', function () {
     document.body.style.backgroundColor = 'black'; // Change back to original color
   }
 });
+
+//parallel scroll
+document.addEventListener('scroll', function () {
+  const topViewport = window.scrollY;
+  const midViewport = topViewport + window.innerHeight / 2;
+
+  //select all container elements
+  const containers = document.querySelectorAll('.container');
+  sections.forEach(section => {
+    const topSection = section.offsetTop;
+    const midSection = topSection + section.offsetHeight / 2;
+    const distanceToSection = midViewport - midSection;
+    // console.log(distanceToSection);
+
+    const parallaxTags = section.querySelectorAll(`[data-parallax]`)
+
+    parallaxTags.forEach(tag => {
+      const speed = tag.getAttribute('data-parallax')
+      tag.style.transform = `translate(0, ${distanceToSection * speed}px)`
+    })
+  })
+})
